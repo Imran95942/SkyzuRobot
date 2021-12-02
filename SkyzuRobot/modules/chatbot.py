25bd0e97f3aea73539a332c427e4b9a72319f35f
Imran95942 <89960334+Imran95942@users.noreply.github.com>
2021-12-02 21:07:36 +0300

    Update chatbot.py

--- SkyzuRobot/modules/chatbot.py
+++ SkyzuRobot/modules/chatbot.py
@@ -194,20 +194,21 @@ async def hmm(client, message):
                 return
         # test = emoji.demojize(test.strip())
 
-        test = test.replace("Skyzu", "Aco")
-        test = test.replace("Skyzu", "Aco")
-        test = test.replace("My god is @skyzu", "I'm a Christian")
+        test = test.replace("BORZ", "Aco")
+        test = test.replace("BORZ", "Aco")
+        test = test.replace("мой Создатель @isIam07", "I'm a Чеченец")
         test = test.replace("16", "9")
-        test = test.replace("@skyzu is my husband.", "I'm single.")
-        test = test.replace("@skyzusupport", "skyzubot.ai")
-        test = test.replace("I live in @skyzusupport.", "I live in San Francisco, California")
-        test = test.replace("I was created by @skyzu", "I made myself")
+        test = test.replace("@isIam07 мой Разработчик.", "У меня никого нет.")
+        test = test.replace("меня разработал @isIam07", "I'm single.")
+        test = test.replace("@isIam07", "BORZ.ai")
+        test = test.replace("Меня разработал @isIam07.", "I live in San Francisco, California.")
+        test = test.replace("I was created by @isIam07", "I made myself")
         test = test.replace(
-            "Hello there I am Skyzu...nice to meet u",
+            "Hello there I am BORZ...nice to meet u",
             "Hi, my friend! Do you want me to tell you a joke?")
-        test = test.replace("@skyzu is my owner", "Have the control right.")
+        test = test.replace("@isIam07 is my owner", "Have the control right.")
         test = test.replace(
-            "Hi, My name is Skyzu Nice to meet you.",
+            "Hi, My name is BORZ Nice to meet you.",
             "Hi, my friend, what can I do for you today?")
         response = await lunaQuery(
             test, message.from_user.id if message.from_user else 0
@@ -289,21 +290,22 @@ async def inuka(client, message):
             test = test.text
         except:
             return
-    test = test.replace("Skyzu", "Aco")
-    test = test.replace("Skyzu", "Aco")
-    test = test.replace("My god is @skyzu", "I'm a Christian")
-    test = test.replace("16", "9")
-    test = test.replace("@skyzu is my husband.", "I'm single.")
-    test = test.replace("@skyzusupport", "Skyzubot.ai")
-    test = test.replace("I live in @skyzusupport.", "I live in San Francisco, California.")
-    test = test.replace("I was created by @skyzu", "I made myself")
-    test = test.replace(
-        "Hello there I am Skyzu...nice to meet u",
-        "Hi, my friend! Do you want me to tell you a joke?")
-    test = test.replace("@skyzu is my owner", "Have the control right.")
-    test = test.replace(
-        "Hi, My name is Skyzu Nice to meet you.",
-        "Hi, my friend, what can I do for you today?")
+    test = test.replace("BORZ", "Aco")
+        test = test.replace("BORZ", "Aco")
+        test = test.replace("мой Создатель @isIam07", "I'm a Чеченец")
+        test = test.replace("16", "9")
+        test = test.replace("@isIam07 мой Разработчик.", "У меня никого нет.")
+        test = test.replace("меня разработал @isIam07", "I'm single.")
+        test = test.replace("@isIam07", "BORZ.ai")
+        test = test.replace("Меня разработал @isIam07.", "I live in San Francisco, California.")
+        test = test.replace("I was created by @isIam07", "I made myself")
+        test = test.replace(
+            "Hello there I am BORZ...nice to meet u",
+            "Hi, my friend! Do you want me to tell you a joke?")
+        test = test.replace("@isIam07 is my owner", "Have the control right.")
+        test = test.replace(
+            "Hi, My name is BORZ Nice to meet you.",
+            "Hi, my friend, what can I do for you today?")
 
     response = await lunaQuery(test, message.from_user.id if message.from_user else 0)
     response = response.replace("Aco", "Skyzu")
@@ -336,7 +338,7 @@ async def inuka(client, message):
         return
 
 
-@pbot.on_message(filters.regex("Skyzu|Skyzu|robot|Skyzu|kiju") & ~filters.bot & ~filters.via_bot  & ~filters.forwarded & ~filters.reply & ~filters.channel & ~filters.edited)
+@pbot.on_message(filters.regex("BORZ|BORZ|robot|BORZ|kiju") & ~filters.bot & ~filters.via_bot  & ~filters.forwarded & ~filters.reply & ~filters.channel & ~filters.edited)
 async def inuka(client, message):
     msg = message.text
     if msg.startswith("/") or msg.startswith("@"):
@@ -384,21 +386,22 @@ async def inuka(client, message):
 
     # test = emoji.demojize(test.strip())
 
-    test = test.replace("Skyzu", "Aco")
-    test = test.replace("Skyzu", "Aco")
-    test = test.replace("My god is @skyzu", "I'm a Christian")
-    test = test.replace("16", "9") 
-    test = test.replace("@skyzu is my husband.", "I'm single.")
-    test = test.replace("@skyzusupport", "Skyzubot.ai")
-    test = test.replace("I live in @Skyzusupport.", "I live in San Francisco, California.")
-    test = test.replace("I was created by @skyzu", "I made myself")
-    test = test.replace(
-        "Hello there I am Skyzu...nice to meet u",
-        "Hi, my friend! Do you want me to tell you a joke?")
-    test = test.replace("@skyzu is my owner", "Have the control right.")
-    test = test.replace(
-        "Hi, My name is Skyzu Nice to meet you.",
-        "Hi, my friend, what can I do for you today?")
+    test = test.replace("BORZ", "Aco")
+        test = test.replace("BORZ", "Aco")
+        test = test.replace("мой Создатель @isIam07", "I'm a Чеченец")
+        test = test.replace("16", "9")
+        test = test.replace("@isIam07 мой Разработчик.", "У меня никого нет.")
+        test = test.replace("меня разработал @isIam07", "I'm single.")
+        test = test.replace("@isIam07", "BORZ.ai")
+        test = test.replace("Меня разработал @isIam07.", "I live in San Francisco, California.")
+        test = test.replace("I was created by @isIam07", "I made myself")
+        test = test.replace(
+            "Hello there I am BORZ...nice to meet u",
+            "Hi, my friend! Do you want me to tell you a joke?")
+        test = test.replace("@isIam07 is my owner", "Have the control right.")
+        test = test.replace(
+            "Hi, My name is BORZ Nice to meet you.",
+            "Hi, my friend, what can I do for you today?")
     response = await lunaQuery(test, message.from_user.id if message.from_user else 0)
     response = response.replace("Aco", "Skyzu")
     response = response.replace("aco", "Skyzu")
